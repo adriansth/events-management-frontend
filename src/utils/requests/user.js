ac99e1549dd2c077deb9e92e8adc253025eaed43
adriansth <soto.herrera.adrian@gmail.com>
2024-10-14 12:36:19 -0600

    feat: token into user requests

--- src/utils/requests/user.js
+++ src/utils/requests/user.js
@@ -2,13 +2,34 @@ import axios from "axios";
 
 const baseUrl = "http://127.0.0.1:8000";
 
-export const createUser = async (uid, firstName, lastName, email) => {
+export const createUser = async (token, uid, firstName, lastName, email) => {
    try {
-      const response = await axios.post(`${baseUrl}/user/`, {
-         uid: uid,
-         first_name: firstName,
-         last_name: lastName,
-         email: email,
+      const response = await axios.post(
+         `${baseUrl}/user/`,
+         {
+            uid: uid,
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+         },
+         {
+            headers: {
+               Authorization: `Bearer ${token}`,
+            },
+         }
+      );
+      return response.data;
+   } catch (err) {
+      console.error(err.message);
+   }
+};
+
+export const getUserByUid = async (token, uid) => {
+   try {
+      const response = await axios.get(`${baseUrl}/user/${uid}`, {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
       });
       return response.data;
    } catch (err) {
@@ -16,27 +37,26 @@ export const createUser = async (uid, firstName, lastName, email) => {
    }
 };
 
-export const getUserByUid = async (uid) => {
+export const updateUserByUid = async (token, uid, updates) => {
    try {
-      const response = await axios.get(`${baseUrl}/user/${uid}`);
+      const response = await axios.put(`${baseUrl}/user/${uid}`, updates, {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
+      });
       return response.data;
    } catch (err) {
       console.error(err.message);
    }
 };
 
-export const updateUserByUid = async (uid, updates) => {
+export const deleteUserByUid = async (token, uid) => {
    try {
-      const response = await axios.put(`${baseUrl}/user/${uid}`, updates);
-      return response.data;
-   } catch (err) {
-      console.error(err.message);
-   }
-};
-
-export const deleteUserByUid = async (uid) => {
-   try {
-      const response = await axios.delete(`${baseUrl}/user/${uid}`);
+      const response = await axios.delete(`${baseUrl}/user/${uid}`, {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
+      });
       return response.data;
    } catch (err) {
       console.error(err.message);
